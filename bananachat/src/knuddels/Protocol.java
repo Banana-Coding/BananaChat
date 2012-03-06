634477430f514ec701656c7e3b10a3e8bfca6952
Fif <youfif@gmail.com>
2012-03-06 12:07:56 +0100

    Unnoetige Kommentare entfernt und Readme aktualisiert

--- bananachat/src/knuddels/Protocol.java
+++ bananachat/src/knuddels/Protocol.java
@@ -47,35 +47,9 @@ public class Protocol {
 	}
 
 	public static byte[] encode(byte[] message) {
-		int length = message.length - 1;
-		byte[] len;
-
-		if (length < 128) {
-			len = new byte[] { (byte) length };
-		} else {
-			int count = 0;
-
-			while (32 << (count + 1 << 3) <= length) {
-				count++;
-			}
-
-			count++;
-			len = new byte[count + 1];
-			len[0] = (byte) (count << 5 | 0x80 | length & 0x1F);
-
-			for (int i = 1; i < len.length; i++) {
-				len[i] = (byte) (length >>> 8 * (i - 1) + 5);
-			}
-		}
-
-		byte[] buffer = new byte[len.length + message.length];
-		System.arraycopy(len, 0, buffer, 0, len.length);
-		System.arraycopy(message, 0, buffer, len.length, message.length);
-
 		try {
 			return Huffman.encode(new String(message, "UTF8"));
 		} catch (UnsupportedEncodingException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 			return null;
 		}

9ad00aceadbc7825e8cff1c0ae4fd2bbebaa62d8
Tobias <youfif@gmail.com>
2012-05-26 11:35:06 +0200

    Fehler behoben
    Es gab einige Fehler, welche durch falscher Syncronisation entstanden
    sind.

--- bananachat/src/knuddels/Protocol.java
+++ bananachat/src/knuddels/Protocol.java
@@ -9,23 +9,17 @@
  * stammt - nur zu Lernzwecken, das Hosten des originalen Clients
  * ist untersagt und wird der Knuddels GmbH gemeldet.
  */
+
 package knuddels;
 
-import java.io.IOException;
-<<<<<<< HEAD
 import java.io.InputStream;
-=======
->>>>>>> parent of 395f65e... Huffman-Kompremierung hinzugefuegt
+import java.io.IOException;
 
 /**
  *
  * @author Flav
  */
 public class Protocol {
-<<<<<<< HEAD
-
-=======
->>>>>>> parent of 395f65e... Huffman-Kompremierung hinzugefuegt
     public static byte[] decode(InputStream in) throws IOException {
         byte first = (byte) in.read();
 
@@ -40,55 +34,6 @@ public class Protocol {
         } else {
             length = (first & 0x1F) + 1;
             int count = (first & 0x60) >>> 5;
-<<<<<<< HEAD
-
-            for (int i = 0; i < count; i++) {
-                length += in.read() << (i << 3) + 5;
-            }
-        }
-
-        byte[] buffer = new byte[length];
-        in.read(buffer, 0, length);
-//        String decoded = Huffman.decode(buffer);
-//        System.out.println("[>] " + decoded);
-//        return decoded.getBytes();
-        return buffer;
-    }
-
-    public static byte[] encode(byte[] message) {
-        int length = message.length - 1;
-		byte[] len;
-
-		if (length < 128) {
-		    len = new byte[]{(byte) length};
-		} else {
-		    int count = 0;
-
-		    while (32 << (count + 1 << 3) <= length) {
-		        count++;
-		    }
-
-		    count++;
-		    len = new byte[count + 1];
-		    len[0] = (byte) (count << 5 | 0x80 | length & 0x1F);
-
-		    for (int i = 1; i < len.length; i++) {
-		        len[i] = (byte) (length >>> 8 * (i - 1) + 5);
-		    }
-		}
-
-		byte[] buffer = new byte[len.length + message.length];
-		System.arraycopy(len, 0, buffer, 0, len.length);
-		System.arraycopy(message, 0, buffer, len.length, message.length);
-//            byte[] encoded = Huffman.encode(new String(buffer, "UTF8"));
-////
-//            String decoded = Huffman.decode(encoded);
-//            System.out.println("[<] " + decoded);
-//           return encoded;
-		return buffer;
-    }
-}
-=======
 
             for (int i = 0; i < count; i++) {
                 length += in.read() << (i << 3) + 5;
@@ -128,4 +73,3 @@ public class Protocol {
         return buffer;
     }
 }
->>>>>>> parent of 395f65e... Huffman-Kompremierung hinzugefuegt
--- bananachat/src/knuddels/SessionHandler.java
+++ bananachat/src/knuddels/SessionHandler.java
@@ -17,68 +17,71 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.Socket;
 
+import tools.huffman.Huffman;
+
 /**
- *
+ * 
  * @author Flav
  */
 public class SessionHandler extends Thread {
-    private Socket socket;
+	private Socket socket;
 
-    public SessionHandler(Socket socket) {
-        this.socket = socket;
-    }
+	public SessionHandler(Socket socket) {
+		this.socket = socket;
+	}
 
-    @Override
-    public void run() {
-        Client client = new Client(socket);
+	@Override
+	public void run() {
+		Client client = new Client(socket);
 
-        try {
-            InputStream in = socket.getInputStream();
-            byte type = (byte) in.read();
+		try {
+			InputStream in = socket.getInputStream();
+			byte type = (byte) in.read();
 
-            if (type == 0x00) {
-<<<<<<< HEAD
+			if (type == 0x00) {
 				while (true) {
-                    byte[] buffer = Protocol.decode(in);
-                    String[] tokens = Huffman.decode(buffer).split("\0");
+					byte[] buffer = Protocol.decode(in);
+					String[] tokens = Huffman.getDecoder().decode(buffer).split("\0");
 					String opcode = tokens[0];
-=======
-                while (true) {
-                    byte[] buffer = Protocol.decode(in);
-                    String[] tokens = new String(buffer, "UTF8").split("\u0000");
-                    String opcode = tokens[0];
->>>>>>> parent of 395f65e... Huffman-Kompremierung hinzugefuegt
 
-                    if (opcode.equals(ReceiveOpcode.EXCEPTION.getValue())) {
-                    } else if (opcode.equals(ReceiveOpcode.DISCONNECT.getValue())) {
-                    } else if (opcode.equals(ReceiveOpcode.CHAT.getValue())) {
-                        ChatHandler.handle(tokens, client);
-                    } else if (opcode.equals(ReceiveOpcode.PING.getValue())) {
-                        PingHandler.handle(tokens, client);
-                    } else if (opcode.equals(ReceiveOpcode.POLL.getValue())) {
-                        PollHandler.handle(tokens, client);
-                    } else if (opcode.equals(ReceiveOpcode.LINK_CLICKED.getValue())) {
-                    } else if (opcode.equals(ReceiveOpcode.JOIN_CHANNEL.getValue())) {
-                        JoinChannelHandler.handle(tokens, client);
-                    } else if (opcode.equals(ReceiveOpcode.Q_TOKEN.getValue())) {
-                    } else if (opcode.equals(ReceiveOpcode.REQUEST_USER_LIST.getValue())) {
-                    } else if (opcode.equals(ReceiveOpcode.HANDSHAKE.getValue())) {
-                        HandshakeHandler.handle(tokens, client);
-                    } else if (opcode.equals(ReceiveOpcode.REQUEST_HELP.getValue())) {
-                        RequestHelpHandler.handle(tokens, client);
-                    } else if (opcode.equals(ReceiveOpcode.LEAVE_CHANNEL.getValue())) {
-                        LeaveChannelHandler.handle(tokens, client);
-                    } else if (opcode.equals(ReceiveOpcode.WHOIS.getValue())) {
-                    } else {
-                        System.out.println(String.format("Unhandled opcode: %s", opcode));
-                    }
-                }
-            } else if (type == 0x02) {
-                // registration
-            }
-        } catch (IOException e) {
-        } finally {
-            client.disconnect();
-        }
-    }
+					if (opcode.equals(ReceiveOpcode.EXCEPTION.getValue())) {
+					} else if (opcode.equals(ReceiveOpcode.DISCONNECT
+							.getValue())) {
+					} else if (opcode.equals(ReceiveOpcode.CHAT.getValue())) {
+						ChatHandler.handle(tokens, client);
+					} else if (opcode.equals(ReceiveOpcode.PING.getValue())) {
+						PingHandler.handle(tokens, client);
+					} else if (opcode.equals(ReceiveOpcode.POLL.getValue())) {
+						PollHandler.handle(tokens, client);
+					} else if (opcode.equals(ReceiveOpcode.LINK_CLICKED
+							.getValue())) {
+					} else if (opcode.equals(ReceiveOpcode.JOIN_CHANNEL
+							.getValue())) {
+						JoinChannelHandler.handle(tokens, client);
+					} else if (opcode.equals(ReceiveOpcode.Q_TOKEN.getValue())) {
+					} else if (opcode.equals(ReceiveOpcode.REQUEST_USER_LIST
+							.getValue())) {
+					} else if (opcode
+							.equals(ReceiveOpcode.HANDSHAKE.getValue())) {
+						HandshakeHandler.handle(tokens, client);
+					} else if (opcode.equals(ReceiveOpcode.REQUEST_HELP
+							.getValue())) {
+						RequestHelpHandler.handle(tokens, client);
+					} else if (opcode.equals(ReceiveOpcode.LEAVE_CHANNEL
+							.getValue())) {
+						LeaveChannelHandler.handle(tokens, client);
+					} else if (opcode.equals(ReceiveOpcode.WHOIS.getValue())) {
+					} else {
+						System.out.println(String.format(
+								"Unhandled opcode: %s", opcode));
+					}
+				}
+			} else if (type == 0x02) {
+				// registration
+			}
+		} catch (IOException e) {
+		} finally {
+			client.disconnect();
+		}
+	}
 }
